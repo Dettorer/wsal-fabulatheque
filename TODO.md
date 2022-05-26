f1c153248016a96942c23309091da602df55b675
Paul Dettorer Hervot <dettorer@dettorer.net>
2022-05-26 15:09:35 +0200

    learnpress: rename sensors to allow putting everything in the same file

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+## Bugs
+
+- It seems we can't change the `LearnPressSensor.php` file name, it's been
+  identified by wsal as a mandatory file and I can't find how. This file
+  contains all sensors, not only LearnPress' ones, so the name is misleading,
+  but can't be changed.
--- wp-security-audit-log/custom-sensors/LearnPressSensor.php
+++ wp-security-audit-log/custom-sensors/LearnPressSensor.php
@@ -1,7 +1,7 @@
 <?php // phpcs:disable WordPress.Files.FileName.NotHyphenatedLowercase
 
 /**
- * Custom Sensors for the LearnPress plugin.
+ * Custom Sensors for the fabulatheque website.
  *
  * Class file for alert manager.
  *
@@ -15,21 +15,24 @@ class WSAL_Sensors_LearnPressSensor extends WSAL_AbstractSensor {
      * Hook events related to the sensor.
      */
     public function HookEvents() {
+        /*
+         * LearnPress sensors
+         */
         // courses and lessons
-        add_action( 'learnpress/user/course-enrolled', array( $this, 'LogCourseEnroll' ), 10, 3 );
-        add_action( 'learn-press/user-completed-lesson', array( $this, 'LogLessonComplete' ), 10, 3 );
-        add_action( 'learn-press/user-course-finished', array( $this, 'LogCourseFinished' ), 10, 3 );
+        add_action( 'learnpress/user/course-enrolled', array( $this, 'LogLPCourseEnroll' ), 10, 3 );
+        add_action( 'learn-press/user-completed-lesson', array( $this, 'LogLPLessonComplete' ), 10, 3 );
+        add_action( 'learn-press/user-course-finished', array( $this, 'LogLPCourseFinished' ), 10, 3 );
 
         // quizzes
-        add_action( 'learn-press/user/quiz-started', array( $this, 'LogQuizStarted' ), 10, 3 );
-        add_action( 'learn-press/user/quiz-finished', array( $this, 'LogQuizFinished' ), 10, 3 );
-        add_action( 'learn-press/user/quiz-retried', array( $this, 'LogQuizRetried' ), 10, 3 );
+        add_action( 'learn-press/user/quiz-started', array( $this, 'LogLPQuizStarted' ), 10, 3 );
+        add_action( 'learn-press/user/quiz-finished', array( $this, 'LogLPQuizFinished' ), 10, 3 );
+        add_action( 'learn-press/user/quiz-retried', array( $this, 'LogLPQuizRetried' ), 10, 3 );
     }
 
     /**
      * Log user enrollment to LearnPress courses
      */
-    public function LogCourseEnroll( $order_id, $course_id, $user_id ) {
+    public function LogLPCourseEnroll( $order_id, $course_id, $user_id ) {
         $alert_code = 1010000;
 
         // Very important: these variable will also show up in the wsal_metadata
@@ -46,7 +49,7 @@ class WSAL_Sensors_LearnPressSensor extends WSAL_AbstractSensor {
     /**
      * Log user progress to LearnPress courses: when they complete a lesson
      */
-    public function LogLessonComplete( $lesson_id, $course_id, $user_id ) {
+    public function LogLPLessonComplete( $lesson_id, $course_id, $user_id ) {
         $alert_code = 1010001;
 
         // Very important: these variable will also show up in the wsal_metadata
@@ -64,7 +67,7 @@ class WSAL_Sensors_LearnPressSensor extends WSAL_AbstractSensor {
     /**
      * Log users finishing a LearnPress course
      */
-    public function LogCourseFinished( $course_id, $user_id, $return_status ) {
+    public function LogLPCourseFinished( $course_id, $user_id, $return_status ) {
         $alert_code = 1010002;
 
         // Very important: these variable will also show up in the wsal_metadata
@@ -81,7 +84,7 @@ class WSAL_Sensors_LearnPressSensor extends WSAL_AbstractSensor {
     /**
      * Log a LearnPress quiz-related event
      */
-    private function LogQuizEvent( $alert_code, $quiz_id, $course_id, $user_id ) {
+    private function LogLPQuizEvent( $alert_code, $quiz_id, $course_id, $user_id ) {
         // Very important: these variable will also show up in the wsal_metadata
         // database table.
         $variables = array(
@@ -97,21 +100,21 @@ class WSAL_Sensors_LearnPressSensor extends WSAL_AbstractSensor {
     /**
      * Log users starting a quiz in LearnPress course
      */
-    public function LogQuizStarted( ...$args ) {
-        $this->LogQuizEvent( 1010003, ...$args );
+    public function LogLPQuizStarted( ...$args ) {
+        $this->LogLPQuizEvent( 1010003, ...$args );
     }
 
     /**
      * Log users starting a quiz in LearnPress course
      */
-    public function LogQuizFinished( ...$args ) {
-        $this->LogQuizEvent( 1010004, ...$args );
+    public function LogLPQuizFinished( ...$args ) {
+        $this->LogLPQuizEvent( 1010004, ...$args );
     }
 
     /**
      * Log users starting a quiz in LearnPress course
      */
-    public function LogQuizRetried( ...$args ) {
-        $this->LogQuizEvent( 1010005, ...$args );
+    public function LogLPQuizRetried( ...$args ) {
+        $this->LogLPQuizEvent( 1010005, ...$args );
     }
 }
